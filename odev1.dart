f68d1ec8bdb9a8fd504223955bfb0ad9ba25cee6
Furkan VAROL <99636501+varolfurkan@users.noreply.github.com>
2023-10-26 15:43:19 +0300

    Changes done.

---
+++ odev1.dart
@@ -0,0 +1,100 @@
+void main() {
+  String ilce = "Aydın";
+  String kita = "Asya";
+  int faks = 12345;
+  String postaKodu = "09100";
+  String instaAdres = "@asd";
+  String calistigiBolum = "Yazılım";
+  int urunMiktari = 20;
+  String  musteriSoyadi = "varol";
+  double odemeMiktari = 129.37;
+  String dogumTarihi = "24.07.1997";
+  double borc = 25345.24;
+  String medeniHal = "Bekar";
+  String videoYorumu = "Çok beğendim";
+  String odemeSaati = "06.17";
+  int eftMiktari = 232;
+  double satilanMiktar = 543.2;
+  String telefonModeli = "iPhone 13";
+  String dergiAdi = "Bilim ve Teknik";
+  String basimTarihi = "26.10.2023";
+  int zamMiktari = 2000;
+  int daireSayisi = 4;
+  int enlem = 36;
+  int boylam = 42;
+  String yemekAdi = "Pizza";
+  double urunFiyati = 47.2;
+  String sirket = "Kariyet.net";
+  String videoAdi = "Beynin sırları";
+  double muzikSuresi = 23.2;
+  double mekanPuani = 4.7;
+  String dosyaAdi = "video.mp4";
+  String resFormati = "jpg";
+  String renk = "Kırmızı";
+  String renkKodu = "#ff0000";
+  String pcModeli = "Asus";
+  double ekranBoyutu = 23.8;
+  double kullanimSuresi = 4.5;
+  double basinc = 2.5;
+  String etkinlikGunu = "Salı";
+  String odemeGunu = "Cuma";
+  String yolculukCikisTarihi = "25.10.2023";
+  String mahalleAdi = "Meşrutiyet";
+  String otobusHatti = "40B";
+  int kullanilanDakika = 123;
+  String kargoTakipNo = "2032384";
+  int kuponSuresi = 30;
+  String kuponKodu = "23asF21";
+  String faturaTarihi = "23.10.2023";
+
+
+
+  print("İlçe: $ilce "
+      "\nKıta: $kita"
+      "\nFaks: $faks"
+      "\nPosta Kodu: $postaKodu"
+      "\nİnsta Adresi: $instaAdres"
+      "\nÇalıştığı Bölüm: $calistigiBolum"
+      "\nÜrün Miktarı: $urunMiktari adet"
+      "\nMüşteri Soyadı: $musteriSoyadi"
+      "\nÖdeme Miktarı: $odemeMiktari TL"
+      "\nDoğum Tarihi: $dogumTarihi"
+      "\nBorç: $borc TL"
+      "\nMedeni Hal: $medeniHal"
+      "\nVideo yorumu: $videoYorumu"
+      "\nÖdeme Saati: $odemeSaati"
+      "\nEft miktarı: $eftMiktari TL"
+      "\nSatılan miktar: $satilanMiktar TL"
+      "\nTelefon modeli: $telefonModeli"
+      "\nDergi adı: $dergiAdi"
+      "\nBasım tarihi: $basimTarihi"
+      "\nZam miktarı: $zamMiktari TL"
+      "\nDaire sayısı: $daireSayisi"
+      "\nEnlem: $enlem"
+      "\nBoylam: $boylam"
+      "\nYemek adı: $yemekAdi"
+      "\nÜrün fiyatı: $urunFiyati TL"
+      "\nŞirket: $sirket"
+      "\nVideo adı: $videoAdi"
+      "\nMüzik süresi: $muzikSuresi dakika"
+      "\nMekan puanı: $mekanPuani yıldız"
+      "\nDosya adı: $dosyaAdi"
+      "\nResim formatı: $resFormati"
+      "\nRenk: $renk"
+      "\nRenk kodu: $renkKodu"
+      "\nBilgisayar modeli: $pcModeli"
+      "\nEkran boyutu: $ekranBoyutu inch"
+      "\nKullanım süresi: $kullanimSuresi saat"
+      "\nBasınç: $basinc atm"
+      "\nEtkinlik günü: $etkinlikGunu"
+      "\nÖdeme günü: $odemeGunu"
+      "\nYolculuk çıkış tarihi: $yolculukCikisTarihi"
+      "\nMahalle adı: $mahalleAdi"
+      "\nOtobüs hattı: $otobusHatti"
+      "\nKullanılan dakika: $kullanilanDakika dakika"
+      "\nKargo takip no: $kargoTakipNo"
+      "\nKupon süresi: $kuponSuresi gün"
+      "\nKupon kodu: $kuponKodu"
+      "\nFatura tarihi: $faturaTarihi"
+  );
+}
